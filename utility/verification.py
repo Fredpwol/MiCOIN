affed4fba8bf7eb3c89f37898abc5743117214e7
Fredpwol <cyborg290@gmail.com>
2021-01-13 22:12:38 +0100

    verified transaction signature

--- utility/verification.py
+++ utility/verification.py
@@ -1,28 +1,30 @@
 from utility.hash_util import hash_block, hash_string_256
-
+from ..wallet import Wallet
 
 class Verification():
     @staticmethod
-    def valid_proof(transaction,last_hash,proof):
+    def valid_proof(transactions,last_hash,proof):
         '''
         This checks if The hash of the proof of work data meets a certain criteria if so
         return True
         '''
-        trial = (str([tx.toordered_dict() for tx in transaction]) + str(last_hash) + str(proof)).encode()
+        trial = (str([tx.toordered_dict() for tx in transactions]) + str(last_hash) + str(proof)).encode()
         hashedtrail = hash_string_256(trial)
-        print(hashedtrail)
         return hashedtrail[0:2] == '00'
 
     @staticmethod
-    def verify_transaction(transaction,get_balance):
+    def verify_transaction(transaction, get_balance, check_balance=True):
         '''
         This is will check if the sender has enough balance left to send to the reciepient
         '''
-        sender_balance = get_balance(transaction.sender)
-        return sender_balance >= transaction.amount
+        if check_balance:
+            sender_balance = get_balance(transaction.sender)
+            return sender_balance >= transaction.amount and Wallet.verify_signature(transaction)
+        else:
+            return Wallet.verify_signature(transaction)
 
     @classmethod
-    def verify_chain(cls,blockchain):
+    def verify_chain(cls, blockchain):
         '''Checks if the chain is Valid or not in a block chain and stop it from corrupting the 
         Entire system....'''
         for (index,block) in enumerate(blockchain):
@@ -38,4 +40,4 @@ class Verification():
 
     @classmethod
     def verify_openTransaction(cls,open_tx,get_balance):
-        return [cls.verify_transaction(tx,get_balance) for tx in open_tx]
+        return [cls.verify_transaction(tx, get_balance, False) for tx in open_tx]
