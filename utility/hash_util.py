33c8c11542c3b9dfc7d2472a7bbfbed3cd862a97
Fredpwol <cyborg290@gmail.com>
2021-01-20 11:59:13 +0100

    refactored code

--- utility/hash_util.py
+++ utility/hash_util.py
@@ -1,6 +1,8 @@
 import hashlib as hl
 import json
 
+# __all__ = ['hash_string_256', 'hash_block']
+
 def hash_string_256(string):
     """Create a SHA256 hash for a given input string.
 
@@ -17,5 +19,5 @@ def hash_block(block):
         :block: The block that should be hashed.
     """
     hashable_block = block.__dict__.copy()
-    hashable_block['transaction'] = [tx.toordered_dict() for tx in hashable_block['transaction']]
+    hashable_block['transactions'] = [tx.to_ordered_dict() for tx in hashable_block['transactions']]
     return hash_string_256(json.dumps(hashable_block, sort_keys=True).encode())
--- utility/verification.py
+++ utility/verification.py
@@ -1,43 +1,55 @@
-from utility.hash_util import hash_block, hash_string_256
+"""Provides verification helper methods."""
+
+from utility.hash_util import hash_string_256, hash_block
 from wallet import Wallet
 
-class Verification():
+class Verification:
+    """A helper class which offer various static and class-based verification and validation methods."""
     @staticmethod
-    def valid_proof(transactions,last_hash,proof):
-        '''
-        This checks if The hash of the proof of work data meets a certain criteria if so
-        return True
-        '''
-        trial = (str([tx.toordered_dict() for tx in transactions]) + str(last_hash) + str(proof)).encode()
-        hashedtrail = hash_string_256(trial)
-        return hashedtrail[0:2] == '00'
+    def valid_proof(transactions, last_hash, proof):
+        """Validate a proof of work number and see if it solves the puzzle algorithm (two leading 0s)
 
-    @staticmethod
-    def verify_transaction(transaction, get_balance, check_balance=True):
-        '''
-        This is will check if the sender has enough balance left to send to the reciepient
-        '''
-        if check_balance:
-            sender_balance = get_balance(transaction.sender)
-            return sender_balance >= transaction.amount and Wallet.verify_signature(transaction)
-        else:
-            return Wallet.verify_signature(transaction)
-
+        Arguments:
+            :transactions: The transactions of the block for which the proof is created.
+            :last_hash: The previous block's hash which will be stored in the current block.
+            :proof: The proof number we're testing.
+        """
+        # Create a string with all the hash inputs
+        guess = (str([tx.to_ordered_dict() for tx in transactions]) + str(last_hash) + str(proof)).encode()
+        # Hash the string
+        # IMPORTANT: This is NOT the same hash as will be stored in the previous_hash. It's a not a block's hash. It's only used for the proof-of-work algorithm.
+        guess_hash = hash_string_256(guess)
+        # Only a hash (which is based on the above inputs) which starts with two 0s is treated as valid
+        # This condition is of course defined by you. You could also require 10 leading 0s - this would take significantly longer (and this allows you to control the speed at which new blocks can be added)
+        return guess_hash[0:2] == '00'
+        
     @classmethod
     def verify_chain(cls, blockchain):
-        '''Checks if the chain is Valid or not in a block chain and stop it from corrupting the 
-        Entire system....'''
-        for (index,block) in enumerate(blockchain):
+        """ Verify the current blockchain and return True if it's valid, False otherwise."""
+        for (index, block) in enumerate(blockchain):
             if index == 0:
                 continue
-            elif block.previous_hash != hash_block(blockchain[index-1]):
-                return  False
-            # checks if the hash of The  block fullfils The condition given else return false
-            if not cls.valid_proof(block.transaction [:-1],block.previous_hash,block.proof):
-                print('Invalid Block!!!')
+            if block.previous_hash != hash_block(blockchain[index - 1]):
+                return False
+            if not cls.valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
+                print('Proof of work is invalid')
                 return False
         return True
 
+    @staticmethod
+    def verify_transaction(transaction, get_balance, check_funds=True):
+        """Verify a transaction by checking whether the sender has sufficient coins.
+
+        Arguments:
+            :transaction: The transaction that should be verified.
+        """
+        if check_funds:
+            sender_balance = get_balance(transaction.sender)
+            return sender_balance >= transaction.amount and Wallet.verify_transaction(transaction)
+        else:
+            return Wallet.verify_transaction(transaction)
+
     @classmethod
-    def verify_openTransaction(cls,open_tx,get_balance):
-        return [cls.verify_transaction(tx, get_balance, False) for tx in open_tx]
+    def verify_transactions(cls, open_transactions, get_balance):
+        """Verifies all open transactions."""
+        return all([cls.verify_transaction(tx, get_balance, False) for tx in open_transactions])
